f04c282a48cc4b924aca7a1c3d5e2d72b242ce4b
David Vidals <david.vidals08@gmail.com>
2021-10-07 17:16:01 -0400

    First Test passed

---
+++ script.js
@@ -0,0 +1,21 @@
+// var button = document.createElement("button");
+// button.innerHTML = "Get Weather";
+// type = "submit";
+
+
+// // 2. Append somewhere
+// var body = document.getElementsByTagName("body")[0];
+// newButton.appendChild(button);
+
+// document.forms['newButton']
+
+var x = document.createElement("INPUT");
+x.setAttribute("type", "text");
+
+// let btn = document.createElement("button");
+// btn.innerHTML = "Get Weather";
+// btn.setAttribute('type',"submit");
+// btn.name = "formBtn";
+
+// newButton.appendChild(x)
+// newButton.appendChild(btn);
